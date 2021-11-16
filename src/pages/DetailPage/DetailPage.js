80de92a1f910037f83060072f5300ee2b9da686b
Elizabeth Jang <elizabethjang@Elizabethui-MacBookPro.local>
2021-11-16 21:17:38 +0900

    Add: 상세페이지 레이아웃 구현
    Refactor: 리뷰 반영
    - json 파일 내 숫자데이터 변경
    - useState, useEffect 변수명 변경
    - bottom-up 방식 최대한 반영
    
    Add: slick slider 라이브러리 활용하여 슬라이더 구현

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+import ProductHead from './Components/ProductHead';
+import StarRating from './Components/StarRating';
+import Review from './Components/Review';
+import ReviewInputBox from './Components/ReviewInputBox';
 
 export default function DetailPage() {
-  return <div />;
+  return (
+    <DetailPageWrapper>
+      <ProductHead />
+      <StarRating />
+      <ReviewsSection>
+        <Review />
+      </ReviewsSection>
+      <ReviewInputBox />
+      <ProductBody>
+        <BodyTitle>프렙 소개</BodyTitle>
+        <BodyDescription>블라블라블라</BodyDescription>
+      </ProductBody>
+    </DetailPageWrapper>
+  );
 }
+
+const DetailPageWrapper = styled.div`
+  width: 900px;
+  margin: 50px auto;
+`;
+
+const ReviewsSection = styled.section`
+  margin-top: 30px;
+  padding: 25px;
+  background-color: rgb(245, 250, 254);
+  border-top: 1px solid rgb(223, 236, 253);
+  border-bottom: 1px solid rgb(223, 236, 253);
+`;
+
+const ProductBody = styled.section`
+  margin: 30px auto;
+  border-top: 2px solid ${({ theme }) => theme.subColor};
+`;
+
+const BodyTitle = styled.h2`
+  margin: 30px 0;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const BodyDescription = styled.p`
+  margin: 0 20px;
+`;
